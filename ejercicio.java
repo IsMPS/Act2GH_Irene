165d2da63dd691b94a12354350f3777686562574
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-18 14:09:21 +0100

    Añado fichero ejercicio.java

---
+++ ejercicio.java
@@ -0,0 +1,9 @@
+public class ejercicio{
+    public static void main(String[] args) {
+        int[] array=new int[20];
+
+        for (int i=0; i<array.length; i++){
+            array[i]=(int)(Math.random()*10+1);
+        }
+    }
+}
