a9f2a01c41f0cdfb7c2ff98132b82d13000e9bb9
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-25 13:00:40 +0100

    Cambio en el archivo ejercicio en el que el array ahora añadimos 30 numeros

--- ejercicio.java
+++ ejercicio.java
@@ -1,6 +1,6 @@
 public class ejercicio{
     public static void main(String[] args) {
-        int[] array=new int[20];
+        int[] array=new int[30];
 
         for (int i=0; i<array.length; i++){
             array[i]=(int)(Math.random()*10+1);
